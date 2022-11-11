6b6225a55ff3b8c64eff90930af92c28f9e2c9bb
dylanvanzantencognizant <dylan.vanzanten@cognizant.com>
2022-11-11 09:27:16 +0100

    feat(Header): Add Header component, add svgr webpack for rendering svg elements

---
+++ components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+
+// Resources
+import Logo from "/public/logo.svg";
+
+// Component
+const Header = ({}) => {
+  useEffect(() => {
+    const header = document.querySelector(".header");
+    if (!header) return;
+
+    const isSticky = () => {
+      const scrollTop = window.scrollY;
+
+      scrollTop >= 50
+        ? header.classList.add("header--sticky")
+        : header.classList.remove("header--sticky");
+    };
+
+    window.addEventListener("scroll", isSticky);
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  });
+
+  return (
+    <header className="header">
+      <div className="container">
+        <span className="header__logo">
+          <Logo />
+          <a className="header__home-link" href="">
+            Go home
+          </a>
+        </span>
+      </div>
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  navigation: true,
+};
+
+export default Header;
