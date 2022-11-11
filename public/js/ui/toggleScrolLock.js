99f3c9e1187af903891c57d26f11e17a2058ede3
dylanvanzantencognizant <dylan.vanzanten@cognizant.com>
2022-11-11 11:00:36 +0100

    feat(): Add toggleScrolLock to prevent scrolling when having menu open on mobile

---
+++ public/js/ui/toggleScrolLock.js
@@ -0,0 +1,42 @@
+/* JS solution since needed on iOS https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/ */
+
+let scrollY = 0;
+
+const toggleScrollLock = (isLocked) => {
+  const siteNav = document.body.querySelector("[data-site-nav]");
+  const siteNavToggle = document.body.querySelector("[data-site-nav-toggle]");
+
+  if (isLocked) {
+    scrollY = window.pageYOffset;
+    document.body.style.right = "0px";
+    document.body.style.left = "0px";
+    document.body.style.position = "fixed";
+    document.body.dataset.hasScrollLock = true;
+
+    if (!siteNav || !siteNavToggle) {
+      return;
+    }
+    siteNav.style.transform = `translate(-100%, ${scrollY}px)`;
+    siteNavToggle.style.transform = `translateY(${scrollY}px)`;
+  } else {
+    document.body.style.position = "";
+    document.body.style.top = "";
+    document.body.style.right = "";
+    document.body.style.left = "";
+
+    document.querySelector("html").setAttribute("data-scroll-hijacking", true);
+    window.scroll(0, scrollY);
+    document
+      .querySelector("html")
+      .removeAttribute("data-scroll-hijacking", true);
+    document.body.dataset.hasScrollLock = false;
+
+    if (!siteNav || !siteNavToggle) {
+      return;
+    }
+    siteNav.style.transform = "";
+    siteNavToggle.style.transform = "";
+  }
+};
+
+export default toggleScrollLock;
