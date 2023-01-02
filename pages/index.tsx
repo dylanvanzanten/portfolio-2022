f36b5946cff4950fde025bbe0fe79ab53a98b951
dylanvanzanten <info@dylanvanzanten.nl>
2023-01-02 09:45:00 +0100

    chore(): Code cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 
+// Components
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import CaseTeaser from "../components/CaseTeaser/CaseTeaser";
@@ -9,9 +10,6 @@ export default function Home() {
     <>
       <Head>
         <title></title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta property="og:locale" content="" />
-        <meta property="og:site_name" content="" />
       </Head>
       <Header />
       <Hero />
