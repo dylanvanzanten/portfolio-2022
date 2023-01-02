5304eeaf56fc52c57aab2f5830d890c48c356e6a
dylanvanzanten <info@dylanvanzanten.nl>
2023-01-02 09:45:26 +0100

    refactor(): Add favicon and meta tags to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
+import Head from "next/head";
+
 import "../styles/_all.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:locale" content="" />
+        <meta property="og:site_name" content="" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
