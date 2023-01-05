02b146945915ec84dd4b810f5cb5d68dbfa11a32
dylanvanzanten <info@dylanvanzanten.nl>
2023-01-05 09:45:50 +0100

    feat(Button): Add Button component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,79 @@
+// Libs
+import React from "react";
+import PropTypes from "prop-types";
+
+import Link from "next/link";
+
+// Utils
+
+// Resources
+
+// Components
+
+// Component
+const Button = ({
+  classNames,
+  element,
+  label,
+  type,
+  buttonType,
+  href,
+  tstId,
+  icon,
+  handleClick,
+  target,
+  isLoading,
+  isDisabled,
+  ...attributes
+}) => {
+  return (
+    <>
+      {label && type === "icon" && <label>{label}</label>}
+      <Link
+        className={`button ${
+          type ? `button--${type}` : ""
+        } ${classNames}`.trim()}
+        type={buttonType || undefined}
+        href={href}
+        data-test-id={tstId}
+        onClick={handleClick}
+        target={target}
+        disabled={isDisabled || isLoading}
+        {...attributes}
+      >
+        {label && type !== "icon" && label}
+        <span className="button__icon">{icon && icon}</span>
+      </Link>
+    </>
+  );
+};
+
+// Props
+Button.defaultProps = {
+  classNames: "",
+  label: "",
+  buttonType: "",
+  href: "",
+  handleClick: function defaultFunction() {},
+  isLoading: false,
+};
+
+const { bool, func, string, object } = PropTypes;
+
+Button.propTypes = {
+  classNames: string,
+  element: string,
+  label: string,
+  type: string,
+  buttonType: string,
+  href: string,
+  tstId: string,
+  icon: object,
+  handleClick: func,
+  target: string,
+  isLoading: bool,
+  disabled: string,
+  isDisabled: bool,
+};
+
+export default Button;
