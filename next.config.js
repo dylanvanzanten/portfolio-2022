ab43cb08d7089344664359983cfd0d8ea0ea78bb
dylanvanzanten <info@dylanvanzanten.nl>
2023-04-15 16:26:30 +0200

    feat(): Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -46,4 +49,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+(module.exports = nextConfig), withBundleAnalyzer({});
